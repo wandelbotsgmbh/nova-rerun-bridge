148f85437bf4120855fae52aa59ee6ff0ad59c5e
Stefan Wagner <stefan.wagner@wandelbots.com>
2025-02-05 15:49:57 +0100

    add collision-free PTP planning with additional obstacle handling

--- nova_rerun_bridge/examples/13_collision_free_p2p.py
+++ nova_rerun_bridge/examples/13_collision_free_p2p.py
@@ -1,12 +1,8 @@
 import asyncio
 
 import numpy as np
-from nova import MotionSettings
-from nova.actions import Linear, ptp
 from nova.api import models
-from nova.core.exceptions import PlanTrajectoryFailed
 from nova.core.nova import Nova
-from nova.types import Pose
 from wandelbots_api_client.models import PlanCollisionFreePTPRequest
 
 from nova_rerun_bridge import NovaRerunBridge
@@ -26,6 +22,15 @@ async def build_collision_world(
         )
     )
     await collision_api.store_collider(cell=cell_name, collider="workpiece", collider2=collider)
+
+    # define annoying obstacle
+    sphere_collider = models.Collider(
+        shape=models.ColliderShape(models.Sphere2(radius=100, shape_type="sphere")),
+        pose=models.Pose2(position=[-100, -400, 200]),
+    )
+    await collision_api.store_collider(
+        cell=cell_name, collider="annoying_obstacle", collider2=sphere_collider
+    )
 
     # define TCP collider geometry
     tool_collider = models.Collider(
@@ -47,7 +52,7 @@ async def build_collision_world(
 
     # assemble scene
     scene = models.CollisionScene(
-        colliders={"workpiece": collider},
+        colliders={"workpiece": collider, "annoying_obstacle": sphere_collider},
         motion_groups={
             "motion_group": models.CollisionMotionGroup(
                 tool={"tool_geometry": tool_collider}, link_chain=robot_link_colliders
@@ -76,7 +81,7 @@ async def test():
             "cell", controller[0].motion_group_id, "Flange"
         )
 
-        await build_collision_world(nova, "cell", robot_setup)
+        collision_scene_id = await build_collision_world(nova, "cell", robot_setup)
 
         await bridge.log_collision_scenes()
 
@@ -87,22 +92,34 @@ async def test():
             home = await motion_group.tcp_pose(tcp)
             home_joints = await motion_group.joints()
 
+            """
             actions = [
-                action
-                for _ in range(3)
-                for action in [
-                    ptp(home),
-                    Linear(target=Pose((50, 20, 30, 0, 0, 0)) @ home),
-                    Linear(target=Pose((100, 20, 30, 0, 0, 0)) @ home),
-                    Linear(target=Pose((50, 20, 30, 0, 0, 0)) @ home),
-                    ptp(home),
-                ]
+                ptp(home),
+                Linear(target=Pose((-100, 0, 30, 0, 0, 0)) @ home),
+                Linear(target=Pose((-100, 0, -600, 0, 0, 0)) @ home),
+                Linear(target=Pose((-100, 0, 30, 0, 0, 0)) @ home),
+                ptp(home),
             ]
 
             for action in actions:
                 action.settings = MotionSettings(tcp_velocity_limit=200)
 
+            try:
+                joint_trajectory = await motion_group.plan(actions, tcp)
+                await bridge.log_actions(actions)
+                await bridge.log_trajectory(joint_trajectory, tcp, motion_group)
+            except PlanTrajectoryFailed as e:
+                await bridge.log_actions(actions)
+                await bridge.log_trajectory(e.error.joint_trajectory, tcp, motion_group)
+                await bridge.log_error_feedback(e.error.error_feedback)
+            """
+
             # Plan collision free PTP
+            scene_api = nova._api_client.store_collision_scenes_api
+            collision_scene = await scene_api.get_stored_collision_scene(
+                cell="cell", scene=collision_scene_id
+            )
+
             """
             robot_setup: OptimizerSetup = Field(description="The robot setup as returned from [getOptimizerConfiguration](getOptimizerConfiguration) endpoint.")
             start_joint_position: List[Union[StrictFloat, StrictInt]]
@@ -110,25 +127,20 @@ async def test():
             static_colliders: Optional[Dict[str, Collider]] = Field(default=None, description="A collection of identifiable colliders.")
             collision_motion_group: Optional[CollisionMotionGroup] = Field(default=None, description="Collision motion group considered during the motion planning. ")
             """
-            await nova._api_client.motion_api.plan_collision_free_ptp(
-                cell="cell",
-                plan_collision_free_ptp_request=PlanCollisionFreePTPRequest(
-                    robot_setup=robot_setup,
-                    start_joint_position=[1.169, -1.57, 1.36, 1.029, 1.289, 1.279],
-                    target=models.PlanCollisionFreePTPRequestTarget([400, 0, 100, 0, 0, 0]),
-                ),
+            planTrajectory: models.PlanTrajectoryResponse = (
+                await nova._api_client.motion_api.plan_collision_free_ptp(
+                    cell="cell",
+                    plan_collision_free_ptp_request=PlanCollisionFreePTPRequest(
+                        robot_setup=robot_setup,
+                        start_joint_position=home_joints,
+                        target=models.PlanCollisionFreePTPRequestTarget([400, 0, 100, 0, 0, 0]),
+                        static_colliders=collision_scene.colliders,
+                        collision_motion_group=collision_scene.motion_groups["motion_group"],
+                    ),
+                )
             )
-
-            try:
-                joint_trajectory = await motion_group.plan(actions, tcp)
-                await bridge.log_actions(actions)
-                await bridge.log_trajectory(joint_trajectory, tcp, motion_group)
-                await motion_group.execute(joint_trajectory, tcp, actions=actions)
-            except PlanTrajectoryFailed as e:
-                await bridge.log_actions(actions)
-                await bridge.log_trajectory(e.error.joint_trajectory, tcp, motion_group)
-                await bridge.log_error_feedback(e.error.error_feedback)
-                return
+            joint_trajectory = planTrajectory.response.actual_instance.joint_trajectory
+            await bridge.log_trajectory(joint_trajectory, tcp, motion_group)
 
         # await cell.delete_robot_controller(controller.name)
 
